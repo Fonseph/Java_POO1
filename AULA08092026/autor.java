2a22d52d72e9521dbf127624044a1374500515e8
Gabriel_Fonseca <162065186+Fonseph@users.noreply.github.com>
2026-09-08 22:28:29 -0300

    Add Autor class with getters and setters

---
+++ AULA08092026/autor.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package livro;
+
+/**
+ *
+ * @author ifsp
+ */
+public class Autor {
+    int codigo;
+    String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
